36dcfea59f8baeed7ce8b132d3ed0165809724ab
j-dobner <94467093+j-dobner@users.noreply.github.com>
2024-08-12 08:22:18 +0200

    Update 2024-07-17_ml_code_hipscore.R
    added extended example calculations

--- 2024-07-17_ml_code_hipscore.R
+++ 2024-07-17_ml_code_hipscore.R
@@ -1,5 +1,6 @@
 library(caret)
 library(tidyverse)
+library(caretEnsemble)
 set.seed(1337)
 #Load Dataset
 m_list <- readRDS("hipscore_ml_data.rds")
@@ -7,6 +8,7 @@ m_list <- readRDS("hipscore_ml_data.rds")
 intrain_list <- list()
 training_list <- list()
 testing_list <- list()
+gene_set = c("undifferentiated", "endoderm", "ectoderm", "mesoderm")
 for(i in 1:length(unique(gene_set))) {
   intrain_list[[i]] <- createDataPartition(y = unlist(m_list[[i]][,1]), p = 0.7)[[1]]
   training_list[[i]] <- m_list[[i]][intrain_list[[i]],]
@@ -36,7 +38,44 @@ for(i in 1:4){
   results_list[[i]] <- resamples(models_list[[i]])
 }
 #Assess model performance and select best model (models_list[[i]] contains the final trained models)
+#1: undifferentiated; 2: endoderm; 3: ectoderm; 4: mesoderm
 summary(results_list[[1]])
 summary(results_list[[2]])
 summary(results_list[[3]])
 summary(results_list[[4]])
+
+#Perform predictions on delta Ct-normalized values (exemplary values)
+#ranger chosen as final model
+undiff_table <- tibble(SPP1 = c(8,12,5),
+                       NANOG = c(10,14,4),
+                       CNMD = c(6, 13, 6))
+undiff_preds <- predict(models_list[[1]]$ranger, undiff_table, type="prob") |> 
+  pull(X1)
+endo_table <- tibble(GATA6 = c(12,12,5),
+                     EOMES = c(11,13,4),
+                     CER1 = c(10,11,1.2))
+endo_preds <- predict(models_list[[2]]$ranger, endo_table, type="prob") |>
+  pull(X1)
+ecto_table <- tibble(PAX6 = c(13,10,7),
+                     HES5  = c(12,14,8),
+                     PAMR1  = c(16, 12, 7))
+ecto_preds <- predict(models_list[[3]]$ranger, ecto_table, type="prob") |> 
+  pull(X1)
+meso_table <- tibble(HAND1 = c(16,8,7),
+                     APLNR = c(12, 5, 8),
+                     HOXB7 = c(17,7,8))
+meso_preds <-predict(models_list[[4]]$ranger, meso_table, type="prob") |> 
+  pull(X1)
+
+#Calculate hiPSCores
+calculate_hipscore <- function(undiff_preds, endo_preds, ecto_preds, meso_preds) {
+tibble(undiff_preds, endo_preds, ecto_preds, meso_preds) |> 
+  rowwise() |> 
+  dplyr::transmute(hipscore = sum(undiff_preds, endo_preds, ecto_preds, meso_preds)) |> 
+  dplyr::mutate(hipscore_result = case_when(
+    hipscore >3 ~ "PASS",
+    hipscore >2 & hipscore <=3 ~ "WARNING",
+    hipscore <=2 ~ "FAIL"
+  ))
+}
+calculate_hipscore(undiff_preds, endo_preds, ecto_preds, meso_preds)
